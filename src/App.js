44fad910dfed4536b261f74c416aef12f8de731d
roy <aroy.sourov@gmail.com>
2022-01-27 14:05:14 +0600

    navok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
-        <p>styled component
-          
-        </p>
+        <Sidebar/>
     </div>
   );
 }

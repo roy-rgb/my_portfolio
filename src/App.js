f54ae2fd22062b4387f84d459c10a55a0026e1ad
roy <aroy.sourov@gmail.com>
2022-01-29 00:46:18 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import Sidebar from "./components/Sidebar";
+import styled from "styled-components";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
         <Sidebar/>
+        <MainContentStyled >
+
+              <div className="lines">
+              
+                 <div className="line-1"></div>
+                 <div className="line-2"></div>
+                 <div className="line-3"></div>
+                 <div className="line-4"></div>
+              </div>
+
+              <HomePage/>
+
+        </MainContentStyled>
+      
     </div>
   );
 }
+const MainContentStyled = styled.main`
+    position: relative;
+  margin-left: 16.3rem;
+  min-height: 100vh;
+
+  .lines{
+    position:absolute;
+    min-height:100vh;
+    width:100%;
+    display: flex;
+    justify-content: space-evenly;
+
+    .line-1, .line-2, .line-3, .line-4{
+      width: 1px;
+      min-height: 100vh;
+      background-color: var(--border-color);
+    }
+  }
+
+`;
 
 export default App;

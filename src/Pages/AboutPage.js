2e1e5d05d9ef878ed15314545d2763719fd9885e
roy <aroy.sourov@gmail.com>
2022-01-30 00:50:48 +0600

    new start

---
+++ src/Pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import {MainLayout} from '../styles/Layouts';
+
+function AboutPage() {
+  return (
+  <div></div>
+  );
+}
+
+export default AboutPage;
